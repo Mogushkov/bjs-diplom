74c670602ba9ff2f8bf16aee944ff847ca0e20cb
Gapur Mogushkov <gapur@mail.ru>
2020-12-06 16:57:39 +0300

    register

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,5 +1,10 @@
 "use strict";
+
 let user = new UserForm();
 user.loginFormCallback = function(data) {
     ApiConnector.login(data, response => response.success ? location.reload() : this.setLoginErrorMessage(response.error));
+};
+
+user.registerFormCallback = function(data) {
+    ApiConnector.register(data, response => response.success ? location.reload() : this.setRegisterErrorMessage(response.error));
 }
